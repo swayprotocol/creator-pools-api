a604a97f2acdbabbdf7f25ac5c3e041d960c32ed
Miha Žnidar <mayki.znidar@gmail.com>
2022-04-20 12:50:53 +0100

    Added framework, added automatic swagger documentation

---
+++ README.md
@@ -0,0 +1,45 @@
+## App
+App is accessible on:
+http://localhost:3000/
+
+## Documentation
+Swagger documentation is accessible:
+http://localhost:3000/api
+
+## Description
+Backend part of Creator pools application, writen in Node.js with Nest framework, using MongoDB as database.
+
+Read [Nest](https://github.com/nestjs/nest) documentation [here](https://docs.nestjs.com/).
+
+## Installation
+
+```bash
+$ yarn install
+```
+
+## Running the app
+
+```bash
+# development
+$ yarn start
+
+# watch mode
+$ yarn start:dev
+
+# production mode
+$ yarn start:prod
+```
+
+## Test
+
+```bash
+# unit tests
+$ yarn test
+
+# e2e tests
+$ yarn test:e2e
+
+# test coverage
+$ yarn test:cov
+```
+
---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const port = 3000;
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Creator pools API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
+}
+bootstrap();
