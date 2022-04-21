af6a8f53f7d0fea5572048b0af8f06b99b90bc99
Miha Žnidar <mayki.znidar@gmail.com>
2022-04-21 10:34:54 +0100

    Env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MONGO_URL } from './config';
+
+const database = MongooseModule.forRoot(MONGO_URL);
 
 @Module({
-  imports: [],
+  imports: [database],
   controllers: [AppController],
   providers: [AppService],
 })
