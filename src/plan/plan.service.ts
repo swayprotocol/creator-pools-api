bcd8af49edbe3eb0bdaa6dda4b7acc6cc5ef305c
Miha Žnidar <mayki.znidar@gmail.com>
2022-04-22 10:37:12 +0100

    Add return type to plan services

--- src/plan/plan.service.ts
+++ src/plan/plan.service.ts
@@ -9,23 +9,23 @@ export class PlanService {
 
   constructor(@InjectModel('Plan') private readonly planModel: Model<Plan>){}
 
-  async create(createPlanDto: CreatePlanDto) {
+  async create(createPlanDto: CreatePlanDto): Promise<Plan> {
     const plan = new this.planModel(createPlanDto)
     await plan.save();
     return plan;
   }
 
-  async findAll() {
+  async findAll(): Promise<Plan[]> {
     const plans = await this.planModel.find();
     return plans;
   }
 
-  async findOne(id: string) {
+  async findOne(id: string): Promise<Plan> {
     const plan = await this.planModel.findOne({_id: id});
     return plan;
   }
 
-  async update(id: string, updatePlanDto: CreatePlanDto) {
+  async update(id: string, updatePlanDto: CreatePlanDto): Promise<Plan> {
     const plan = await this.planModel.findOneAndUpdate(
       { _id: id },
       updatePlanDto,
@@ -34,7 +34,7 @@ export class PlanService {
     return plan;
   }
 
-  async remove(id: string) {
+  async remove(id: string): Promise<Plan> {
     const plan = await this.planModel.findOneAndRemove({_id: id})
     return plan;
   }
