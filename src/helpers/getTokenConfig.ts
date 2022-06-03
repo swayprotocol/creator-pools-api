f82f0663203e7ebfd8397d306594091bcdc249a1
Miha Žnidar <mayki.znidar@gmail.com>
2022-06-03 11:02:20 +0100

    Add config import

--- src/helpers/getTokenConfig.ts
+++ src/helpers/getTokenConfig.ts
@@ -1,5 +1,6 @@
 import '../shared/configs/stagingConfig.json'
 import '../shared/configs/productionConfig.json'
+import '../shared/configs/dualPoolsConfig.json'
 
 export const getTokenConfig = async (name: string) => {
   const config = await require(`../shared/configs/${name}Config.json`)
