45ce5388eb16948bb04abe33408b1c4a63e60057
Miha Žnidar <mayki.znidar@gmail.com>
2022-06-10 12:27:49 +0100

    Unstake clean-up

--- src/unstake/unstake.module.ts
+++ src/unstake/unstake.module.ts
@@ -1,18 +1,12 @@
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { UnstakeService } from './unstake.service';
 import { UnstakeController } from './unstake.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { unstakeSchema } from './entities/unstake.schema';
-import { PoolModule } from '../pool/pool.module';
-import { ClaimModule } from '../claim/claim.module';
-import { StakeModule } from '../stake/stake.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{name: 'Unstake', schema: unstakeSchema}], 'ourDb'),
-    PoolModule,
-    forwardRef(() => ClaimModule),
-    StakeModule,
   ],
   controllers: [UnstakeController],
   providers: [UnstakeService],
--- src/unstake/unstake.service.ts
+++ src/unstake/unstake.service.ts
@@ -1,8 +1,6 @@
-import { forwardRef, Inject, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { Contract, utils } from 'ethers';
-import { Claim } from '../claim/entities/claim.entity';
 import { CreateUnstakeDto } from './dto/create-unstake.dto';
 import { Unstake } from './entities/unstake.entity';
 
@@ -34,17 +32,6 @@ export class UnstakeService {
 
   async findByHash(hash: string): Promise<Unstake> {
     const unstake = await this.unstakeModel.findOne({ hash });
-    return unstake;
-  }
-
-  async pushClaim(id: string, claim: Claim): Promise<Unstake> {
-    const unstake = await this.unstakeModel.findOneAndUpdate({
-        _id: id
-      },{
-        $push: { claims: claim }
-      },
-      { new: true }
-    )
     return unstake;
   }
 }
