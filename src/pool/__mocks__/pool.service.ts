d5b8e5cd0eca253ec1d20852b161e3a863808d40
Miha Žnidar <mayki.znidar@gmail.com>
2022-07-22 13:45:24 +0200

    Pool mock and stub

--- src/pool/__mocks__/pool.service.ts
+++ src/pool/__mocks__/pool.service.ts
@@ -2,8 +2,10 @@ import { poolStub } from "../tests/pool.stub";
 
 export const PoolService = jest.fn().mockResolvedValue({
   create: jest.fn().mockResolvedValue(poolStub()),
+  findAllAfter: jest.fn().mockResolvedValue([poolStub()]),
   findAll: jest.fn().mockResolvedValue([poolStub()]),
   findOne: jest.fn().mockResolvedValue(poolStub()),
-  update: jest.fn().mockResolvedValue(poolStub()),
-  remove: jest.fn().mockResolvedValue(poolStub())
+  findByHash: jest.fn().mockResolvedValue(poolStub()),
+  findOneByHandle: jest.fn().mockResolvedValue(poolStub()),
+  updateByHandle: jest.fn().mockResolvedValue(poolStub()),
 })
