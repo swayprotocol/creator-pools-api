13cb5a6a8bc1ca754b14fec2416f285026dfe282
Miha Žnidar <mayki.znidar@gmail.com>
2022-07-22 13:45:44 +0200

    Claim mock, stub and tests

--- src/claim/tests/claim.controller.spec.ts
+++ src/claim/tests/claim.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ClaimController } from '../claim.controller';
 import { ClaimService } from '../claim.service';
+import { Claim } from '../entities/claim.entity';
+import { claimStub } from './claim.stub';
 
 jest.mock('../claim.service');
 
@@ -21,4 +23,38 @@ describe('ClaimController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  describe('findAll', () => {
+    let claims: Claim[];
+
+    beforeEach(async () => {
+      claims = await controller.findAll();
+    })
+    test('should return all claims', async () => {
+      expect(claims).toEqual([claimStub()])
+    })
+  })
+
+  describe('findOne', () => {
+    let claim: Claim;
+    let stub: Claim;
+
+    beforeEach(async () => {
+      stub = claimStub();
+      claim = await controller.findOne(stub._id)
+    })
+    test('should return claim with same id', async () => {
+      expect(claim).toEqual(stub);
+    })
+  })
+
+  describe('totalRewards', () => {
+    let totalRewards: number
+    beforeEach(async () => {
+      totalRewards = await controller.getTotalRewards();
+    })
+    test('should return all claims total reward', async () => {
+      expect(totalRewards).toEqual(Number)
+    })
+  })
 });
